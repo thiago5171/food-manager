5382681bc63f56600d2780a956ed1eccc3042347
felixismael72 <felixismael72@gmail.com>
2022-12-07 19:12:13 -0300

    Setup view structure

---
+++ app/src/main/java/view/Main.kt
@@ -0,0 +1,12 @@
+package view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.food_manager.R
+
+class Main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/view/food_technical_sheet/RegisterForm.kt
+++ app/src/main/java/view/food_technical_sheet/RegisterForm.kt
@@ -1,12 +1,12 @@
-package view
+package view.food_technical_sheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.food_manager.R
 
-class Dashboard : AppCompatActivity() {
+class RegisterForm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.activity_register_form)
     }
 }
