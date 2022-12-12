665973a9290e8ee3f36878fef8847de5749d8d3f
felixismael72 <felixismael72@gmail.com>
2022-12-12 19:10:46 -0300

    Add integration between main and register food tech sheet

--- app/src/main/java/view/Main.kt
+++ app/src/main/java/view/Main.kt
@@ -1,12 +1,20 @@
 package view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.food_manager.R
+import view.food_technical_sheet.RegisterForm
 
 class Main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.redirect_button)
+        button.setOnClickListener {
+            val intent = Intent(this, RegisterForm::class.java)
+            startActivity(intent)
+        }
     }
 }
