d240277096f58dd5229db0a23ef6c6d1eb8251e0
felixismael72 <felixismael72@gmail.com>
2022-12-18 00:21:40 -0300

    Setup main activity

--- app/src/main/java/view/Main.kt
+++ app/src/main/java/view/Main.kt
@@ -3,7 +3,7 @@ package view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.LinearLayout
 import com.example.food_manager.R
 import view.food_technical_sheet.RegisterForm
 
@@ -11,8 +11,8 @@ class Main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.redirect_button)
-        button.setOnClickListener {
+        val linearLayout = findViewById<LinearLayout>(R.id.recipes)
+        linearLayout.setOnClickListener {
             val intent = Intent(this, RegisterForm::class.java)
             startActivity(intent)
         }
