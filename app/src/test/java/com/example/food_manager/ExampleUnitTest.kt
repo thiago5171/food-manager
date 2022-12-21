79de00d7d19fce0b24d59c0ff680fafc85f808f2
thiago5171 <tgazaroli@gmail.com>
2022-12-21 01:08:06 -0300

    Add news  unit adapter tests

--- app/src/test/java/com/example/food_manager/ExampleUnitTest.kt
+++ app/src/test/java/com/example/food_manager/ExampleUnitTest.kt
@@ -5,6 +5,7 @@ import androidx.test.core.app.ApplicationProvider
 import com.example.food_manager.domain.RecipeItemListModel
 import com.example.food_manager.domain.adapter.RecipeListAdapter
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Test
 import org.mockito.Mockito.mock
 
@@ -18,6 +19,7 @@ class ExampleUnitTest {
     var url =
         "https://assets.unileversolutions.com/recipes-v2/214590.jpg"
    val context = mock(Context::class.java)
+   val adapter = RecipeListAdapter(context,recipes = recipes)
 
     @Test
     fun addition_isCorrect() {
@@ -26,7 +28,6 @@ class ExampleUnitTest {
 
     @Test
     fun success_get_item_id(){
-        println("context")
         recipes.add(
             RecipeItemListModel(
                 "Torta de frango sem queijoTorta de frango sem queijoTorta de frango sem queijo",
@@ -35,10 +36,38 @@ class ExampleUnitTest {
                 url
             )
         )
-
-        val adapter = RecipeListAdapter(context,recipes = recipes)
         val expected : Long = 0
         assertEquals(expected,adapter.getItemId(0) )
 
     }
+
+    @Test
+    fun failed_get_item_id(){
+        assertNotEquals( 1, adapter.getItemId(0) )
+
+    }
+
+
+    @Test
+    fun success_get_count(){
+        recipes.add(
+            RecipeItemListModel(
+                "Torta de frango sem queijoTorta de frango sem queijoTorta de frango sem queijo",
+                "Especial do artur corno",
+                24.00,
+                url
+            )
+        )
+        val expected = 1
+        assertEquals(expected,adapter.getCount() )
+
+    }
+
+    @Test
+    fun failed_get_count(){
+        val expected = 1
+        assertNotEquals(expected,adapter.getCount() )
+
+    }
+
 }
