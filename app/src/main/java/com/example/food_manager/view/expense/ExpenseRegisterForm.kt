b7c278a9d1b4f6fdfba2c538bd02cece0b4380a4
JoaoVitorBernardino <jvitorbernardino@gmail.com>
2022-12-23 21:02:30 -0300

    create activity expense

---
+++ app/src/main/java/com/example/food_manager/view/expense/ExpenseRegisterForm.kt
@@ -0,0 +1,12 @@
+package com.example.food_manager.view.expense
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.food_manager.R
+
+class ExpenseRegisterForm : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_expense_register_form)
+    }
+}
