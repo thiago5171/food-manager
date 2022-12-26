1f1aec6a78b7e6d17c1a5c22d84bb6c282727cac
JoaoVitorBernardino <jvitorbernardino@gmail.com>
2022-12-26 16:49:19 -0300

    update activity expense

--- app/src/main/java/com/example/food_manager/view/Main.kt
+++ app/src/main/java/com/example/food_manager/view/Main.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
 import com.example.food_manager.R
-import com.example.food_manager.view.recipe.RecipeRegisterForm
+import com.example.food_manager.view.expense.ExpenseRegisterForm
 import com.example.food_manager.view.recipe.list.RecipeList
 
 class Main : AppCompatActivity() {
@@ -15,6 +15,12 @@ class Main : AppCompatActivity() {
         val recipesLayout = findViewById<LinearLayout>(R.id.recipes)
         recipesLayout.setOnClickListener {
             val intent = Intent(this, RecipeList::class.java)
+            startActivity(intent)
+        }
+
+        val expenseLayout =findViewById<LinearLayout>(R.id.finances)
+        expenseLayout.setOnClickListener {
+            val intent = Intent(this, ExpenseRegisterForm::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/food_manager/view/expense/ExpenseRegisterForm.kt
+++ app/src/main/java/com/example/food_manager/view/expense/ExpenseRegisterForm.kt
@@ -1,10 +1,10 @@
 package com.example.food_manager.view.expense
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
 import android.os.Bundle
 import com.example.food_manager.R
 
-class ExpenseRegisterForm : AppCompatActivity() {
+class ExpenseRegisterForm : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expense_register_form)
