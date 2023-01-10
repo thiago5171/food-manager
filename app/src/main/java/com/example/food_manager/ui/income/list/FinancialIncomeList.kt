ee0dc16e942f67082998fc8bc1de0aa6b807f051
felixismael72 <felixismael72@gmail.com>
2023-01-09 23:45:52 -0300

    Add income simple operations

--- app/src/main/java/com/example/food_manager/ui/income/list/FinancialIncomeList.kt
+++ app/src/main/java/com/example/food_manager/ui/income/list/FinancialIncomeList.kt
@@ -1,8 +1,18 @@
 package com.example.food_manager.ui.income.list
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.food_manager.data.DatabaseHelper
 import com.example.food_manager.databinding.ActivityFinancialIncomeListBinding
+import com.example.food_manager.domain.income.Income
+import com.example.food_manager.ui.adapter.IncomesAdapter
+import com.example.food_manager.ui.income.IncomeRegisterForm
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class FinancialIncomeList : AppCompatActivity() {
     private val binding by lazy {
@@ -12,5 +22,50 @@ class FinancialIncomeList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val dao = DatabaseHelper.getInstance(this).incomeDAO()
+        val scope = MainScope()
+
+        val list = binding.incomesList
+        scope.launch {
+            val incomes = withContext(Dispatchers.IO) {
+                dao.findAll()
+            }
+
+            val incomeList = ArrayList<Income>()
+            incomeList.addAll(incomes)
+            val adapter = IncomesAdapter(incomeList, dao)
+
+            list.layoutManager = GridLayoutManager(
+                this@FinancialIncomeList, GridLayoutManager.VERTICAL)
+            list.adapter = adapter
+        }
+
+        val addIncomeButton = binding.createIncome
+        addIncomeButton.setOnClickListener {
+            val intent = Intent(this, IncomeRegisterForm::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        val dao = DatabaseHelper.getInstance(this).incomeDAO()
+        val scope = MainScope()
+
+        val list = binding.incomesList
+        scope.launch {
+            val incomes = withContext(Dispatchers.IO) {
+                dao.findAll()
+            }
+
+            val incomeList = ArrayList<Income>()
+            incomeList.addAll(incomes)
+            val adapter = IncomesAdapter(incomeList, dao)
+
+            list.layoutManager = GridLayoutManager(
+                this@FinancialIncomeList, GridLayoutManager.VERTICAL)
+            list.adapter = adapter
+        }
     }
 }
