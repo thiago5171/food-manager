6f0175e6e94d8bc807f5158fe932cfd76a0d0aeb
JoaoVitorBernardino <jvitorbernardino@gmail.com>
2023-01-10 16:39:48 -0300

    Update domain

--- app/src/main/java/com/example/food_manager/domain/Expense/Expense.kt
+++ app/src/main/java/com/example/food_manager/domain/Expense/Expense.kt
@@ -1,6 +1,5 @@
 package com.example.food_manager.domain.Expense
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import java.io.Serializable
@@ -8,8 +7,8 @@ import java.io.Serializable
 @Entity
 data class Expense(
     @PrimaryKey(autoGenerate = true) val id : Long = 0L,
-    @ColumnInfo(name = "name") val name: String,
-    @ColumnInfo(name = "description") val description: String,
-    @ColumnInfo(name = "price") val price: Double,
-    @ColumnInfo(name = "imgUri") val imgUri: String
+    val name: String,
+    val description: String,
+    val price: Double,
+    val imgUri: String
 ) : Serializable
